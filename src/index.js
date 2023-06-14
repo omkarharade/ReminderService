f912ac1bae58edc7d54923f1b4b8c30f717fbcb6
Omkar Harade <omkarharade1301@gmail.com>
2023-06-14 21:07:44 +0530

    rabbit mq basic setup complete

--- src/index.js
+++ src/index.js
@@ -6,17 +6,22 @@ const { sendBasicEmail } = require("./services/email-service");
 const jobs = require("./utils/job");
 const TicketController = require("./controllers/ticket-controller");
 
-const setupAndStartServer = () => {
+const { subscribeMessage, createChannel } = require("./utils/messageQueue");
+const { REMINDER_BINDING_KEY } = require("./config/serverConfig");
+
+const setupAndStartServer = async () => {
 	const app = express();
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: true }));
 
 	app.post("/api/v1/tickets", TicketController.create);
 
+	const channel = await createChannel();
+	subscribeMessage(channel, undefined, REMINDER_BINDING_KEY);
 	app.listen(PORT, () => {
 		console.log(`Server started at port : ${PORT}`);
 
-		jobs();
+		// jobs();
 		// sendBasicEmail(
 		// 	"support@admin.com",
 		// 	"booking.notification.services@gmail.com",
